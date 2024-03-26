a93ca6911df6e134dbac551c0c5ebf75609b6981
Abhishek Waghmare <49281527+abhiwaghmare@users.noreply.github.com>
2024-03-26 16:58:58 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,21 @@
+// const heading = React.createElement("h1", {id:"heading"}, "Hello world from React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  [React.createElement(
+    "div",
+    { id: "child1" },
+    [React.createElement("h1", {}, "I am tag h1 from child1"),React.createElement("h2", {}, "I am tag h2 from child1")]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    [React.createElement("h1", {}, "I am tag h1 from child2"),React.createElement("h2", {}, "I am tag h2 from child2")]
+  )]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
